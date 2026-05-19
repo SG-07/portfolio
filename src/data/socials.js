a0cce21630fdc47821aa04fb99bc289b29c5cdfe
Satyam Gangwar <gangwar.satyam01@gmail.com>
2026-05-19 08:25:52 +0530

    Created Centeralized Socails

---
+++ src/data/socials.js
@@ -0,0 +1,27 @@
+import {
+  FaGithub,
+  FaLinkedin,
+  FaTwitter,
+  FaEnvelope,
+} from "react-icons/fa";
+
+export const socials = [
+  {
+    id: 1,
+    name: "GitHub",
+    icon: FaGithub,
+    url: "https://github.com/SG-07",
+  },
+  {
+    id: 2,
+    name: "LinkedIn",
+    icon: FaLinkedin,
+    url: "https://www.linkedin.com/in/satyamgangwar",
+  },
+  {
+    id: 3,
+    name: "Email",
+    icon: FaEnvelope,
+    url: "mailto:gangwar.satyam01@gmail.com",
+  },
+];
